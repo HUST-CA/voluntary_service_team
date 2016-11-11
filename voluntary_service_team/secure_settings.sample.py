250cae3ef626c52a73772bff56adee847f8edda1
Hamster Tian <haotia@gmail.com>
2016-11-11 22:56:47 +0800

    Update setting sample to reflect changes in message sender
    * Also remove unused email settings.

--- voluntary_service_team/secure_settings.sample.py
+++ voluntary_service_team/secure_settings.sample.py
@@ -1,13 +1,3 @@
-EMAIL_HOST = 'smtp.excited.ha'
-EMAIL_HOST_USER = 'mogician'
-EMAIL_HOST_PASSWORD = '1708'
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
-
 # Cloud messaging API
-APPKEY = ''
-SECRET = ''
-
-SMS_TEMPLATE_CODE_INFORM = ''
-SMS_TEMPLATE_CODE_PROBLEM = ''
-SMS_TEMPLATE_CODE_SUCCESS = ''
+APIKEY = ''
+TPL_ID = {'receive': '', 'finish': '', 'trouble': ''}
