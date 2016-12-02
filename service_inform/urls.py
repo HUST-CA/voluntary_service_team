0b4fdfa62112331e30f39ffbe522f57199b94438
HUSTCA <engineering@hustca.com>
2016-12-02 22:28:21 +0800

    urls: Raise priority for post_the_feedback
    * Specific ones come first

--- service_inform/urls.py
+++ service_inform/urls.py
@@ -6,6 +6,6 @@ app_name = 'service_inform'
 urlpatterns = [
     url('^service/$', views.ServiceFormView.as_view(), name='service'),
     url('^service/(?P<tel>\d*?)/(?P<pk>\d*?)/$', views.OwnFlagView.as_view(), name='own_flag'),
-    url('^(?P<short_link>.*?)/$', views.ShortLinkRedirect.as_view(), name='short_link_redirect'),
     url('^post_the_feedback/$', views.sms_feedback, name='feedback'),
+    url('^(?P<short_link>.*?)/$', views.ShortLinkRedirect.as_view(), name='short_link_redirect'),
 ]
